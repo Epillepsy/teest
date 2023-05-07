9887b6b0b73bacc67442f9c956f9473d43a5019c
osir gueye <osir.gueye@kindredgroup.com>
2023-05-07 16:53:02 +0200

    firsy

---
+++ src/components/card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{cardInfo}}
+<div>
+    <div>
+        {{card.name}}
+    </div>
+    <div>
+        {{card.type}}
+    </div>
+</div>
+        <div>
+            ============================================
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CardComp",
+    props: {
+        cardInfo: {
+            type: Object
+        }
+    }
+}
+</script>
---
+++ src/components/test.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        this is a test {{ testNumber }} 
+        <div v-for="card in dataCard.data" :key="card.id">
+            <cardComponent :cardInfo="card" @click="goToPage(card)"/>
+        </div>
+        <button @click="increment">increment </button>
+    </div>
+</template>
+<script>
+import axios from "axios"
+// const endpoint ="https://db.ygoprodeck.com/api/v7/cardinfo.php"
+import cardComponent from "./card.vue"
+export default {
+    name: "testL",
+    components: {
+        cardComponent
+    },
+    data() {
+        return {
+            testNumber: 0,
+            dataCard: []
+        }
+    },
+    mounted() {
+
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+        .then((response) => {
+            this.dataCard = response.data;
+        });
+    },
+    methods: {
+        add(a, b) {
+            return a + b
+        },
+        increment() {
+            this.testNumber += 1
+        }
+    }
+}
+</script>
